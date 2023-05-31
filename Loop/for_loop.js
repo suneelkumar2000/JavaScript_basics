de0e1101f6693f3167389a39d9964ce3a8972a7b
suneelkumar2000 <128072552+suneelkumar2000@users.noreply.github.com>
2023-05-31 09:28:19 +0530

    Add files via upload

---
+++ Loop/for_loop.js
@@ -0,0 +1,12 @@
+for(let i=1;i<=5;i++){
+    console.log('hello')
+  }
+//print 1 to 5
+let i
+for(i=1;i<=5;i++){
+  console.log(i)
+}
+
+//print 10 to 1
+for(i=10;i>=1;i--)
+  console.log(i)
---
+++ Loop/while_lopp.js
@@ -0,0 +1,13 @@
+console.log('while loop')
+i=0
+while(i>=1){
+  console.log(i)
+  i--;
+}
+
+console.log('do while loop')
+i=0
+do{
+  console.log(i)
+  i--;
+}while(i>=1)
