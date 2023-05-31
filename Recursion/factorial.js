67e749495dbb12638aab58ceb5a2eb472dc9ea49
suneelkumar2000 <128072552+suneelkumar2000@users.noreply.github.com>
2023-05-31 16:04:43 +0530

    Add files via upload

---
+++ Recursion/factorial.js
@@ -0,0 +1,14 @@
+// factorial used to solve recursion
+
+function fact(n)
+{
+    if(n==0)
+        return 1;
+    else
+        return n * fact(n-1);
+}
+
+let num  = 5;
+let result = fact(num);
+
+console.log(result);
---
+++ Recursion/first.js
@@ -0,0 +1,12 @@
+function show(){
+    console.log("Hi");
+}
+function abc(){
+show();//calling one function on another function
+}
+
+// recursion is function which call itself
+function recurs(){
+    console.log("Hi");
+    recurs();
+}
