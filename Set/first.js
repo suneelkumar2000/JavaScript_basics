0b191f331a6b42a41a64990cea9e2d3e8fa776f6
suneelkumar2000 <128072552+suneelkumar2000@users.noreply.github.com>
2023-05-31 16:03:56 +0530

    Add files via upload

---
+++ Set/first.js
@@ -0,0 +1,13 @@
+
+let a= new("bookkeeper");
+console.log(a);
+
+let nums = new set();
+nums.add(3);//to add elements in set
+nums.add(4);
+nums.add(3);
+nums.add("suneel");
+ 
+console.log(nums.has(3));// to check values are present in the set
+console.log(nums.has(2));
+console.log(nums.has("suneel"));
